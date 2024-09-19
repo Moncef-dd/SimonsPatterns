6cc6698152e9d4cfc0ad61acbfe66cfe46461a81
Moncef SAMAI <Moncef.samai.rol2@gmail.com>
2024-09-18 19:08:28 -0500

    minor changes

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
+let buttonColors = [
+    "red", "cyan", "creamy", 
+    "yellow", "blue", "green", 
+    "gray", "orange", "brown"
+]; 
+
 let gameStarted = false;
 document.addEventListener("keypress", function(){
     document.querySelector("h1").innerHTML = " Game Started!"
     gameStarted = true; 
 })
 
+function nextColorChoosen(colorArray){
+    let u = Math.floor(Math.random() * 9) 
+    return colorArray[u]
+}
+//console.log(buttonColors[8])
+
+//console.log(nextSequence()); 
+
+function generatePattern(currentPattern){
+    if(!currentPattern.length){
+        document.querySelector("h1").innerHTML = "New Game has been Started"; 
+        let currentPattern = []; 
+        let nextColor = nextColorChoosen(buttonColors)
+        currentPattern.push(nextColor);
+    }
+    else {
+        document.querySelector('h1').innerHTML = "Great Job!"; 
+
+
+        let nextColor = nextColorChoosen(buttonColors)
+        currentPattern.push(nextColor)
+    }
+
+}
+
+for(let i = 0; i < document.querySelectorAll(".box").length; i++){
+ document.querySelectorAll(".box")[i].addEventListener("click", /*
+    the event listner goes here!
+    */)
+}
 
